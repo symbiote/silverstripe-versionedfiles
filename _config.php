b08c40782584fa9d2b923cfb38844ad0f5bc04af
ajshort <andrewjshort@gmail.com>
2011-02-27 20:19:24 +1100

    BUGFIX: Added a workround for folders not being deleted from the filesystem that contained _versions directories.

--- _config.php
+++ _config.php
@@ -3,5 +3,6 @@
  * @package silverstripe-versionedfiles
  */
 
-Object::add_extension('File',  'VersionedFileExtension');
-Object::add_extension('Image', 'VersionedImageExtension');
+Object::add_extension('File',   'VersionedFileExtension');
+Object::add_extension('Image',  'VersionedImageExtension');
+Object::add_extension('Folder', 'VersionedFolderExtension');
---
+++ code/VersionedFolderExtension.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package silverstripe-versionedfiles
+ */
+class VersionedFolderExtension extends DataObjectDecorator {
+
+	public function onBeforeDelete() {
+		// A workaround for that fact that Folder::onBeforeDelete() only removes
+		// the folder if only a _resampled dir exists. If the folder is empty
+		// and only has _resampled and _versions directories then delete it.
+		if ($this->owner->Filename && is_dir($this->owner->getFullPath())) {
+			$items       = scandir($this->owner->getFullPath());
+			$versionsDir = FileVersion::VERSION_FOLDER;
+
+			// Remove . and .. items.
+			array_shift($items);
+			array_shift($items);
+
+			$delete = (
+				count($items) == 1 && $items[0] == $versionsDir
+				|| count($items) == 2 && $items == array('_resampled', $versionsDir)
+			);
+
+			if ($delete) {
+				Filesystem::removeFolder($this->owner->getFullPath());
+			}
+		}
+	}
+
+}
