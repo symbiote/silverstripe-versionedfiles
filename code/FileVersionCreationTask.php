49989af38564f977a98792075b78cf4c2eb51778
Julian Seidenberg <julian@silverstripe.com>
2013-03-11 12:09:16 +1300

    BUG Fixing creationtask to be compatible with SS 3.1

--- code/FileVersionCreationTask.php
+++ code/FileVersionCreationTask.php
@@ -18,10 +18,8 @@ class FileVersionCreationTask extends BuildTask {
 	public function run($request) {
 		$versionless = DataObject::get(
 			'File',
-			'"File"."ClassName" <> \'Folder\' AND "FileVersion"."FileID" IS NULL',
-			null,
-			'LEFT JOIN "FileVersion" ON "FileVersion"."FileID" = "File"."ID"'
-		);
+			'"File"."ClassName" <> \'Folder\' AND "FileVersion"."FileID" IS NULL')
+				->leftJoin("FileVersion",'"FileVersion"."FileID" = "File"."ID"');
 
 		if($versionless) foreach($versionless as $file) {
 			$file->createVersion();
